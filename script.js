cb9707be90be5e3e12a3b1ca0b86a1186f4ea807
Nitin yadav <92503050+nitin0497@users.noreply.github.com>
2025-07-22 00:03:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   document.querySelectorAll('.dropdown-btn').forEach(btn=>{
     btn.addEventListener('click',()=>{
-      btn.classList.toggle('active');
       const content = btn.nextElementSibling;
       content.classList.toggle('open');
     });
